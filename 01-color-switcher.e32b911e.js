1e66ba84ac628a9a5de4b38fa7e222eaa932082c
DDinkAA <111251262+DDinkAA@users.noreply.github.com>
2023-01-21 13:22:28 +0000

    Deploying to gh-pages from @ DDinkAA/goit-js-hw-09@c2ac74724dc3189774ab15e79f67b075b0f7cfc0 🚀

---
+++ 01-color-switcher.e32b911e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;function n(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){r=setInterval(n,1e3),t.toggleAttribute("disabled"),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.toggleAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.e32b911e.js.map
