c2ac74724dc3189774ab15e79f67b075b0f7cfc0
DDinkAA <qwertdinka@gmail.com>
2023-01-21 15:22:06 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(getBgColor, 1000);
+  startBtn.toggleAttribute('disabled');
+  stopBtn.removeAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.toggleAttribute('disabled');
+}
+
+function getBgColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
